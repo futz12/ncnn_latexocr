d45d877c59c13719d44f188ffbd9bc0dd3eb5ab9
Yexuan Wu <56149058+futz12@users.noreply.github.com>
2025-03-02 12:52:53 +0800

    Add files via upload

---
+++ LatexOCR.cpp
@@ -0,0 +1,13 @@
+﻿//
+// Created by icys on 25-2-10.
+//
+#include <NBLatex/model.h>
+#include <iostream>
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,
+            int nShowCmd)
+{
+    cv::Mat image = cv::imread("./assets/latex.png");
+    NBCapture::LatexOcr model;
+    std::cout << model.forward(image);
+}
